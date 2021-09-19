620185e29cbcbe3cf3aba23622ab315604e79d82
Scolder <69083943+ScolderCreations@users.noreply.github.com>
2021-09-18 19:17:53 -0700

    Update scratchuptime.py

--- scratchuptime.py
+++ scratchuptime.py
@@ -1,6 +1,11 @@
-def check():
+def isUp(): {
   import urllib3
   healthsite = 'https://api.scratch.mit.edu/health'
   http = urllib3.PoolManager()
   r = http.request('GET', healthsite)
-  return(r.json)
+  if '{"connected\":true,\"ready\":true}' in r.text:
+    ret = True
+  else:
+    ret = False
+  return(ret)
+}
